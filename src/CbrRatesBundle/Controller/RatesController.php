abefdc5294c4189de92daf0ef53cca47181a5822
vargas1987 <vargasparyan1987@gmail.com>
2019-06-24 01:34:49 +0300

    create rates controller

--- src/CbrRatesBundle/Controller/RatesController.php
+++ src/CbrRatesBundle/Controller/RatesController.php
@@ -2,26 +2,18 @@
 
 namespace CbrRatesBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/rates")
- */
 class RatesController extends AbstractController
 {
     /**
-     * @Route("/", name="rates-list")
-     * @Template
-     * @param Request $request
-     * @return array|Response
+     * @Route("/rates", name="rates")
      */
-    public function indexAction(Request $request)
+    public function index()
     {
-        return [];
+        return $this->render('rates/index.html.twig', [
+            'controller_name' => 'RatesController',
+        ]);
     }
 }
